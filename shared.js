7aa60fa986690b9597129f45712e770fb7892379
Rose Ludovic <NuggaN85@users.noreply.github.com>
2023-09-09 18:23:57 +0200

    Update shared.js
    J'ai réorganisé le code pour réduire la répétition de code en utilisant une fonction générique 'shareOnSocialMedia'. Cette fonction prend un sélecteur, une URL de partage et un titre en tant que paramètres. Elle attribue ensuite l'événement de clic et appelle la fonction 'popupCenter' avec l'URL de partage appropriée et le titre correspondant. Cela rend le code plus propre et plus facile à maintenir.

--- shared.js
+++ shared.js
@@ -1,89 +1,45 @@
-(function(){
+(function () {
+  var popupCenter = function (url, title, width, height) {
+    var popupWidth = width || 640;
+    var popupHeight = height || 320;
+    var windowLeft = window.screenLeft || window.screenX;
+    var windowTop = window.screenTop || window.screenY;
+    var windowWidth = window.innerWidth || document.documentElement.clientWidth;
+    var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+    var popupLeft = windowLeft + windowWidth / 2 - popupWidth / 2;
+    var popupTop = windowTop + windowHeight / 2 - popupHeight / 2;
+    var popup = window.open(
+      url,
+      title,
+      'scrollbars=yes, width=' +
+        popupWidth +
+        ', height=' +
+        popupHeight +
+        ', top=' +
+        popupTop +
+        ', left=' +
+        popupLeft
+    );
+    popup.focus();
+    return true;
+  };
 
-    var popupCenter = function(url, title, width, height){
-        var popupWidth = width || 640;
-        var popupHeight = height || 320;
-        var windowLeft = window.screenLeft || window.screenX;
-        var windowTop = window.screenTop || window.screenY;
-        var windowWidth = window.innerWidth || document.documentElement.clientWidth;
-        var windowHeight = window.innerHeight || document.documentElement.clientHeight;
-        var popupLeft = windowLeft + windowWidth / 2 - popupWidth / 2 ;
-        var popupTop = windowTop + windowHeight / 2 - popupHeight / 2;
-        var popup = window.open(url, title, 'scrollbars=yes, width=' + popupWidth + ', height=' + popupHeight + ', top=' + popupTop + ', left=' + popupLeft);
-        popup.focus();
-        return true;
-    };
-    
-    document.querySelector('.share_facebook').addEventListener('click', function(e){
-        e.preventDefault();
-        var url = this.getAttribute('data-url');
-        var shareUrl = "//www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent(url);
-        popupCenter(shareUrl, "Partager sur facebook");
+  var shareOnSocialMedia = function (selector, shareUrl, shareTitle) {
+    document.querySelector(selector).addEventListener('click', function (e) {
+      e.preventDefault();
+      var url = this.getAttribute('data-url');
+      popupCenter(shareUrl + encodeURIComponent(url), shareTitle);
     });
+  };
 
-    document.querySelector('.share_twitter').addEventListener('click', function(e){
-        e.preventDefault();
-        var url = this.getAttribute('data-url');
-        var shareUrl = "//twitter.com/intent/tweet?text=" + encodeURIComponent(document.title) +
-            "&via=NuggaN85" +
-            "&url=" + encodeURIComponent(url);
-        popupCenter(shareUrl, "Partager sur Twitter");
-    });
-
-    document.querySelector('.share_gplus').addEventListener('click', function(e){
-        e.preventDefault();
-        var url = this.getAttribute('data-url');
-        var shareUrl = "//plus.google.com/share?url=" + encodeURIComponent(url);
-        popupCenter(shareUrl, "Partager sur Google+");
-    });
-
-    document.querySelector('.share_linkedin').addEventListener('click', function(e){
-        e.preventDefault();
-        var url = this.getAttribute('data-url');
-        var shareUrl = "//www.linkedin.com/shareArticle?url=" + encodeURIComponent(url);
-        popupCenter(shareUrl, "Partager sur Linkedin");
-    });
-    
-    document.querySelector('.share_trumblr').addEventListener('click', function(e){
-        e.preventDefault();
-        var url = this.getAttribute('data-url');
-        var shareUrl = "//www.tumblr.com/widgets/share/tool?shareSource=legacy&canonicalUrl=&url=" +encodeURIComponent(url);
-        popupCenter(shareUrl, "Partager sur Trumblr");
-    });    
-    
-    document.querySelector('.share_pinterest').addEventListener('click', function(e){
-        e.preventDefault();
-        var url = this.getAttribute('data-url');
-        var shareUrl = "//www.pinterest.com/pin/create/button/?url=" +encodeURIComponent(url);
-        popupCenter(shareUrl, "Partager sur pinterest");
-    }); 
-    
-    document.querySelector('.share_reddit').addEventListener('click', function(e){
-        e.preventDefault();
-        var url = this.getAttribute('data-url');
-        var shareUrl = "//www.reddit.com/submit?url=" +encodeURIComponent(url);
-        popupCenter(shareUrl, "Partager sur reddit");
-    }); 
-    
-    document.querySelector('.share_stumbleupon').addEventListener('click', function(e){
-        e.preventDefault();
-        var url = this.getAttribute('data-url');
-        var shareUrl = "//www.stumbleupon.com/submit?url=" +encodeURIComponent(url);
-        popupCenter(shareUrl, "Partager sur stumbleupon");
-    }); 
-    
-    document.querySelector('.share_vk').addEventListener('click', function(e){
-    e.preventDefault();
-    var url = this.getAttribute('data-url');
-    var shareUrl = "//vk.com/share.php?url=" +encodeURIComponent(url);
-    popupCenter(shareUrl, "Partager sur vk");
-    }); 
-    
-    document.querySelector('.share_xing').addEventListener('click', function(e){
-    e.preventDefault();
-    var url = this.getAttribute('data-url');
-    var shareUrl = "//www.xing.com/spi/shares/new?sc_p=b7910_cb&url=" +encodeURIComponent(url);
-    popupCenter(shareUrl, "Partager sur xing");
-    }); 
-     
+  shareOnSocialMedia('.share_facebook', '//www.facebook.com/sharer/sharer.php?u=', 'Partager sur Facebook');
+  shareOnSocialMedia('.share_twitter', '//twitter.com/intent/tweet?text=' + encodeURIComponent(document.title) + '&via=NuggaN85&url=', 'Partager sur Twitter');
+  shareOnSocialMedia('.share_gplus', '//plus.google.com/share?url=', 'Partager sur Google+');
+  shareOnSocialMedia('.share_linkedin', '//www.linkedin.com/shareArticle?url=', 'Partager sur Linkedin');
+  shareOnSocialMedia('.share_tumblr', '//www.tumblr.com/widgets/share/tool?shareSource=legacy&canonicalUrl=&url=', 'Partager sur Tumblr');
+  shareOnSocialMedia('.share_pinterest', '//www.pinterest.com/pin/create/button/?url=', 'Partager sur Pinterest');
+  shareOnSocialMedia('.share_reddit', '//www.reddit.com/submit?url=', 'Partager sur Reddit');
+  shareOnSocialMedia('.share_stumbleupon', '//www.stumbleupon.com/submit?url=', 'Partager sur StumbleUpon');
+  shareOnSocialMedia('.share_vk', '//vk.com/share.php?url=', 'Partager sur VK');
+  shareOnSocialMedia('.share_xing', '//www.xing.com/spi/shares/new?sc_p=b7910_cb&url=', 'Partager sur Xing');
 })();
